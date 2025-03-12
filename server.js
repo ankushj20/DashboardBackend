23171872620791f21d48ffc14c2bc3b26f9a6112
Ankush Jaiswal <ankushjaiswal1111@gmail.com>
2025-03-12 22:35:36 +0530

    mongoDB update and edit sever

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const adRoutes = require("./routes/adRoute");
 
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:5173"], 
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ["http://localhost:5173"], 
+//   credentials: true
+// }));
+
+app.use(cors({ credentials: true, origin: true }));
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
